3aad63985b49b5a9679f66641610ff5675354b37
cmirb <51628604+cmirb@users.noreply.github.com>
2023-06-05 11:47:11 +0200

    worked on exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    return arr.filter(function(item) {
+        return !args.includes(item);
+    });
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    if(first > last) {
+        [first, last] = [last, first];
+    }
+    for(let i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum; 
 };
 
 // Do not edit below this line
